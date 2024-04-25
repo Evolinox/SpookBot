625db421e2687a5b15ed37c29e9541f3b7b96ff0
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-04-25 10:33:12 +0200

    Added Ollama Extension

---
+++ src/main/java/Commands/Ollama.java
@@ -0,0 +1,15 @@
+package Commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Ollama extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+
+        if (event.getName().equals("ollama")) {
+
+        }
+    }
+}
