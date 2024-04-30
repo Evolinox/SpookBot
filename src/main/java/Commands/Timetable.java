77fabd8d049a07005d6f2a1a418cebd963296a9a
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-04-30 15:07:02 +0200

    Added Timetable Response

--- src/main/java/Commands/Timetable.java
+++ src/main/java/Commands/Timetable.java
@@ -2,13 +2,25 @@ package Commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.json.JSONObject;
+import Timetable.Request;
+
+import java.io.IOException;
 
 public class Timetable extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("timetable")) {
-            event.reply("WIP").queue();
+            JSONObject tt = null;
+            try {
+                tt = Request.getTimetable(event.getOption("station").getAsString(), "240430", "15");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            event.reply(tt.toString()).queue();
         }
     }
 }
