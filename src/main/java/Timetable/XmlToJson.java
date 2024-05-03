019e96dcbeb4d56a3219e5c87f05160fc1e6740d
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-05-03 10:33:41 +0200

    Updated XmlToJson.java

--- src/main/java/Timetable/XmlToJson.java
+++ src/main/java/Timetable/XmlToJson.java
@@ -1,14 +1,52 @@
 package Timetable;
 
+import SpookBot.Main;
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-import java.net.http.HttpResponse;
+import javax.xml.parsers.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.http.*;
 
 public class XmlToJson {
     public static JSONObject ToJSON(HttpResponse<String> response) {
-        JSONObject timetable = new JSONObject();
-        timetable.put("station", response.body());
+        // Debug, will be removed, when done.
+        Main.loggingService.info(response.body());
 
+        // Document Builder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            Main.loggingService.severe(String.valueOf(e));
+        }
+
+        // Parse XML
+        InputSource inStream = new InputSource();
+        inStream.setCharacterStream(new StringReader(response.body()));
+        Document document = null;
+        try {
+            document = builder.parse(inStream);
+        } catch (SAXException e) {
+            Main.loggingService.severe(String.valueOf(e));
+        } catch (IOException e) {
+            Main.loggingService.severe(String.valueOf(e));
+        }
+
+        // Get Root Element of XML
+        Element root = document.getDocumentElement();
+
+        // Convert XML to a JSON Object
+        JSONObject timetable = new JSONObject();
+        timetable.put("station", root.getAttribute("station"));
+
+        // Return JSON Object
         return timetable;
     }
 }
