4805831eba6f901978a3a816e3e2ccded429eb47
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-12-18 17:18:15 +0100

    feat(timetable): added more data-entries to json

--- src/main/java/Commands/Timetable.java
+++ src/main/java/Commands/Timetable.java
@@ -3,9 +3,11 @@ package Commands;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import Timetable.Request;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Timetable extends ListenerAdapter {
@@ -16,6 +18,7 @@ public class Timetable extends ListenerAdapter {
             JSONObject timetable = null;
             try {
                 timetable = Request.getTimetable(event.getOption("station").getAsString(), event.getOption("customdate").getAsString(), event.getOption("customhour").getAsString());
+                System.out.println(timetable.toString(1));
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -23,7 +26,17 @@ public class Timetable extends ListenerAdapter {
             // Embed
             EmbedBuilder timetableEmbed = new EmbedBuilder();
             timetableEmbed.setTitle(timetable.optString("station"));
-            timetableEmbed.setDescription("Timetable Plugin V1");
+            timetableEmbed.setFooter("Timetable Plugin V1 · Lokalen Fahrplan beachten!");
+            timetableEmbed.setColor(Color.red);
+
+            // Read Services
+            JSONArray services = timetable.getJSONArray("services");
+            for (int i = 0; i < services.length(); i++) {
+                JSONObject service = services.getJSONObject(i);
+                String title = service.getString("type") + " " + service.getString("line") + " (" + service.getString("number") + ") nach " + service.getString("destination");
+                String data = "Abfahrt von Gleis " + service.getString("track") + "\num 17:42 Uhr";
+                timetableEmbed.addField(title, data, false);
+            }
 
             // Reply
             event.replyEmbeds(timetableEmbed.build()).queue();
--- src/main/java/Timetable/XmlToJson.java
+++ src/main/java/Timetable/XmlToJson.java
@@ -2,6 +2,7 @@ package Timetable;
 
 import SpookBot.Main;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.w3c.dom.*;
 import org.xml.sax.*;
@@ -41,6 +42,55 @@ public class XmlToJson {
         // Convert XML to a JSON Object
         JSONObject timetable = new JSONObject();
         timetable.put("station", root.getAttribute("station"));
+        // Array für Services
+        JSONArray servicesArray = new JSONArray();
+
+        // Get all Services
+        NodeList servicesNodeList = root.getChildNodes();
+        for (int i = 0; i < servicesNodeList.getLength(); i++) {
+            Node serviceNode = servicesNodeList.item(i);
+            String serviceId = "null";
+            String serviceNumber = "null";
+            String serviceLine = "null";
+            String serviceType = "null";
+            String serviceDestination = "null";
+
+            if (serviceNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element serviceElement = (Element) serviceNode;
+                serviceId = serviceElement.getAttribute("id");
+
+                NodeList serviceDataNodeList = serviceElement.getChildNodes();
+
+                for (int j = 0; j < serviceDataNodeList.getLength(); j++) {
+                    Node serviceDataNode = serviceDataNodeList.item(j);
+
+                    if (serviceDataNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element serviceDataElement = (Element) serviceDataNode;
+
+                        if (!serviceDataElement.getAttribute("n").isEmpty()) {
+                            serviceNumber = serviceDataElement.getAttribute("n");
+                        }
+                        if (!serviceDataElement.getAttribute("l").isEmpty()) {
+                            serviceLine = serviceDataElement.getAttribute("l");
+                        }
+                        if (!serviceDataElement.getAttribute("c").isEmpty()) {
+                            serviceType = serviceDataElement.getAttribute("c");
+                        }
+                    }
+                }
+            }
+            // Create JSON Object
+            JSONObject service = new JSONObject();
+            service.put("id", serviceId);
+            service.put("number", serviceNumber);
+            service.put("line", serviceLine);
+            service.put("type", serviceType);
+            service.put("destination", serviceDestination);
+            service.put("track", "2");
+            servicesArray.put(service);
+        }
+
+        timetable.put("services", servicesArray);
 
         // Return JSON Object
         return timetable;
